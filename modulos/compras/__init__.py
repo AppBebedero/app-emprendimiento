dd6f7a966c8ff9bbbbda875453cb1a0686548958
Apps-JoseDanny <alertas.bebedero@gmail.com>
2025-07-18 09:56:50 -0600

    Update __init__.py

--- modulos/compras/__init__.py
+++ modulos/compras/__init__.py
@@ -1,1 +1,8 @@
+from flask import Blueprint, render_template
+
+compras_bp = Blueprint('compras', __name__, template_folder='templates')
+
+@compras_bp.route('/')
+def mostrar_formulario_compras():
+    return render_template('compras.html')
 
