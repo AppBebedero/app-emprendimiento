3c97234c924b54318beadf7731be852daf4c2b9f
Apps-JoseDanny <alertas.bebedero@gmail.com>
2025-07-17 20:24:45 -0600

    Create inicio.py

---
+++ modulos/inicio.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+from config_loader import cargar_configuracion
+
+inicio_bp = Blueprint('inicio', __name__, template_folder='../templates')
+
+@inicio_bp.route('/inicio')
+def mostrar_inicio():
+    config = cargar_configuracion()
+    return render_template('inicio.html', config=config)
