155cfb07ee6b2836684dfbbbcf7abf09a95c2cfd
Apps-JoseDanny <alertas.bebedero@gmail.com>
2025-07-18 10:40:04 -0600

    Update __init__.py

--- modulos/compras/__init__.py
+++ modulos/compras/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 compras_bp = Blueprint('compras', __name__, template_folder='templates')
 
@@ -6,3 +6,12 @@ compras_bp = Blueprint('compras', __name__, template_folder='templates')
 def mostrar_formulario_compras():
     return render_template('compras.html')
 
+@compras_bp.route('/datos_formulario')
+def datos_formulario():
+    datos = {
+        "proveedores": [{"Nombre": "Proveedor 1"}, {"Nombre": "Proveedor 2"}],
+        "productos": [{"Nombre": "Producto A"}, {"Nombre": "Producto B"}],
+        "tipos_compra": ["Contado", "Crédito"],
+        "formas_pago": ["Efectivo", "SINPE", "Tarjeta Cr."]
+    }
+    return jsonify(datos)
