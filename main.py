2329be58a856f6568f76b3ce5ecf749c3e417590
Apps-JoseDanny <alertas.bebedero@gmail.com>
2025-07-18 09:59:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 from config_loader import cargar_configuracion
 
 # Importar blueprints
-from modulos.compras.compras import compras_bp
-from modulos.compras.inicio import inicio_bp
+from modulos.compras import compras_bp
+from modulos.compras.inicio import inicio_bp  # Este depende de si inicio.py existe y tiene un blueprint válido
 
 app = Flask(__name__)
 
