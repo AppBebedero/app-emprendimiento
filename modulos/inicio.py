b5ae95d4485cec03388589abe7e7c9f32ed6a4f0
Apps-JoseDanny <alertas.bebedero@gmail.com>
2025-07-18 13:19:30 -0600

    Update inicio.py

--- modulos/inicio.py
+++ modulos/inicio.py
@@ -3,7 +3,16 @@ from config_loader import cargar_configuracion
 
 inicio_bp = Blueprint('inicio', __name__)
 
-@inicio_bp.route('/inicio')
-def mostrar_inicio():
+@inicio_bp.route('/')
+def inicio():
     config = cargar_configuracion()
-    return render_template('inicio.html', config=config)
+
+    return render_template(
+        'inicio.html',
+        config=config,
+        nombre_negocio=config.get("NombreNegocio", "Sistema Gestión Financiera"),
+        logo_exists=bool(config.get("LogoURL")),
+        logo_url=config.get("LogoURL", ""),
+        color_principal=config.get("ColorPrincipal", "#0d6efd"),
+        color_fondo=config.get("ColorFondo", "#ffffff")
+    )
